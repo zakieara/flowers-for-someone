1dde45d4c216be39de0b45c5191718833af49ecd
zakieara <ahmadzakyamrullah123@gmail.com>
2025-07-31 11:24:35 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Maaf cuma bisa ngasih bunga secara virtual').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
